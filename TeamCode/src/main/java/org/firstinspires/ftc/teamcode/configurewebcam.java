e30c55485126a4fd354b5633ea42a8b2f0564a57
thajan2009 <148616586+thajan2009@users.noreply.github.com>
2025-11-15 10:41:55 +0000

    hiu

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/configurewebcam.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/configurewebcam.java
@@ -63,7 +63,7 @@ import java.util.Locale;
 public class configurewebcam extends LinearOpMode
 {
     /*
-     * EDIT THESE PARAMETERS AS NEEDED
+     * EDIT THESE PARAMETERS AS NEEDED ale
      */
     final boolean USING_WEBCAM = true   ;
     final BuiltinCameraDirection INTERNAL_CAM_DIR = BuiltinCameraDirection.BACK;
